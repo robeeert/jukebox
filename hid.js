4c257f974f9d1987b2418b10d97019973f7cb877
robeeert <theholy13@hotmail.com>
2017-10-14 10:41:41 +0200

    hid

--- hid.js
+++ hid.js
@@ -2,4 +2,8 @@ var HID = require('node-hid');
 
 console.log(HID.devices())
 
-// var device = new HID.HID('\\\\?\\hid#vid_13ba&pid_0018#6&286580aa&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}');
+var device = new HID.HID('0001:0004:00');
+
+device.on("data", function(data) {
+  console.log(data);
+});
