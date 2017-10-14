b0d1e962083ecf094ec9aaae6cea7270f4f84424
robeeert <theholy13@hotmail.com>
2017-10-14 10:42:33 +0200

    hid

--- hid.js
+++ hid.js
@@ -2,7 +2,7 @@ var HID = require('node-hid');
 
 console.log(HID.devices())
 
-var device = new HID.HID('0001:0004:00');
+var device = new HID.HID(5050,24);
 
 device.on("data", function(data) {
   console.log(data);
