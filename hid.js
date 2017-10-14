83fa5abee6998c6642553e11c7599bbdff8c391c
robeeert <theholy13@hotmail.com>
2017-10-14 10:56:51 +0200

    hid

--- hid.js
+++ hid.js
@@ -5,5 +5,7 @@ console.log(HID.devices())
 var device = new HID.HID(5050,24);
 
 device.on("data", function(data) {
-  console.log(data.toString('utf8'));
+  console.log('utf8',data.toString('utf8'));
+  console.log('hex',data.toString('hex'));
+  console.log('base64',data.toString('base64'));
 });
