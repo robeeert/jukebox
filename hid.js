5b57e4b75986f5fc3109983e64e1f1f8881b8651
robeeert <theholy13@hotmail.com>
2018-04-17 10:45:13 +0200

    save

--- hid.js
+++ hid.js
@@ -4,8 +4,18 @@ console.log(HID.devices())
 
 var device = new HID.HID(5050,24);
 
+let combinedData = '';
+
 device.on("data", function(data) {
-  console.log('utf8',data.toString('utf8'));
-  console.log('hex',data.toString('hex'));
-  console.log('base64',data.toString('base64'));
+  let parsedData = parseInt(data.toString('hex'),16);
+  if (parsedData === 0) {
+    test(combinedData);
+    combinedData = '';
+  } else {
+    combinedData + parsedData;
+  }
 });
+
+function test(combinedData) {
+  console.log(combinedData);
+}
