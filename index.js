13a28177495e3ef162b02ec87dea259b419ede12
--- <theholy13@hotmail.com>
2018-09-23 20:13:25 +0200

    test

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ mopidy.on("state:online", function () {
     // mopidy.playlists.getPlaylists().then(pl=>console.log(pl))
 
     console.log("pre lookup")
-    mopidy.library.lookup('spotify:user:126388824:playlist:0DTRXkqYrmvt6lHm3eKX2t').done(test=>{
-        console.log("pre add")
-        mopidy.tracklist.add(test.tracks)
-        console.log("post add")
-    })
+    mopidy.library.lookup('spotify:user:126388824:playlist:0DTRXkqYrmvt6lHm3eKX2t')
+        .then(printTypeAndName)
+        .fold(get, 'tracks')
+        .then(mopidy.tracklist.add)
+        .fold(get, trackNum)
+        .then(mopidy.playback.play)
+        .then(printNowPlaying)
+        .catch(console.error.bind(console))
+        .done();
 
     // console.log("past lookup")
     // mopidy.tracklist.nextTrack().done(test=>console.log(1,"nexttrack"))
@@ -24,6 +28,24 @@ mopidy.on("state:online", function () {
     mopidy.playback.getCurrentTrack()
         .done(printCurrentTrack);
 });
+
+var printTypeAndName = function (model) {
+    console.log(model.__model__ + ": " + model.name);
+    // By returning the playlist, this function can be inserted
+    // anywhere a model with a name is piped in the chain.
+    return model;
+};
+
+var printNowPlaying = function () {
+    // By returning any arguments we get, the function can be inserted
+    // anywhere in the chain.
+    var args = arguments;
+    return mopidy.playback.getCurrentTrack()
+        .then(function (track) {
+            console.log("Now playing:", trackDesc(track));
+            return args;
+        });
+};
 
 var printCurrentTrack = function (track) {
     if (track) {
