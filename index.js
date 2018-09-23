315016f1fad233f191a7823e873ec866a5e9ce40
--- <theholy13@hotmail.com>
2018-09-23 19:38:00 +0200

    test

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ var mopidy = new Mopidy({
 
 
 mopidy.on("state:online", function () {
-    
-    console.log(mopidy.tracklist.add)
 
-    mopidy.tracklist.add([{'uri': 'spotify:track:2gmHWTVT3M598AfBmMD59k'}]);
-    mopidy.playlists.getPlaylists().then(pl=>console.log(pl))
+    // console.log(mopidy.tracklist.add)
 
+    // mopidy.tracklist.add([{'uri': 'spotify:track:2gmHWTVT3M598AfBmMD59k'}]);
+    // mopidy.playlists.getPlaylists().then(pl=>console.log(pl))
+
+    console.log("pre lookup")
     mopidy.library.lookup('spotify:user:126388824:playlist:0DTRXkqYrmvt6lHm3eKX2t').done(test=>mopidy.tracklist.add(test.tracks))
 
-    mopidy.tracklist.nextTrack().done(test=>console.log(test))
+    console.log("past lookup")
+    // mopidy.tracklist.nextTrack().done(test=>console.log(test))
 
     mopidy.playback.getCurrentTrack()
         .done(printCurrentTrack);
