f04296894c6bd34b254fd82aba682c91945ccf8a
--- <theholy13@hotmail.com>
2018-09-23 20:18:27 +0200

    test

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
 var Mopidy = require('mopidy');
 
 var mopidy = new Mopidy({
-    webSocketUrl: "ws://localhost:6680/mopidy/ws/"
+    webSocketUrl: "ws://192.168.2.106:6680/mopidy/ws/"
 });
+
 
 mopidy.on("state:online", function () {
+	console.log("state:online")
+    mopidy.playlists.lookup('spotify:user:126388824:playlist:0DTRXkqYrmvt6lHm3eKX2t').done(test=>console.log(test))
 
-    console.log("pre lookup")
-    mopidy.library.lookup('spotify:user:126388824:playlist:0DTRXkqYrmvt6lHm3eKX2t')
-        .then(printTypeAndName)
-        .fold(get, 'tracks')
-        .then(mopidy.tracklist.add)
-        .fold(get, 0)
-        .then(mopidy.playback.play)
-        .then(printNowPlaying)
-        .catch(console.error.bind(console))
-        .done();
-
+    mopidy.playback.getCurrentTrack()
+        .done(printCurrentTrack);
 });
-
-var get = function (key, object) {
-    return object[key];
-};
-
-var printTypeAndName = function (model) {
-    console.log(model.__model__ + ": " + model.name);
-    // By returning the playlist, this function can be inserted
-    // anywhere a model with a name is piped in the chain.
-    return model;
-};
-
-var printNowPlaying = function () {
-    // By returning any arguments we get, the function can be inserted
-    // anywhere in the chain.
-    var args = arguments;
-    return mopidy.playback.getCurrentTrack()
-        .then(function (track) {
-            console.log("Now playing:", trackDesc(track));
-            return args;
-        });
-};
 
 var printCurrentTrack = function (track) {
     if (track) {
